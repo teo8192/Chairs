8fcfda4706059ff91118b469cdd115e1c20825e4
Teo <teodor@dahlknutsen.no>
2019-02-12 15:40:33 +0100

    fix 20-numebr

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -4,7 +4,10 @@ for reservation in f:
     name, number = reservation.split(",")
     try:
         chairs_per_person = 50 / int(number)
+    except ValueError:
+        print("no number {} ".format(number))
     except:
         print("error ", number)
-    else:
-        print("{} will get {} chairs per person".format(name, chairs_per_person))
+    #else:
+
+        #print("{} will get {} chairs per person".format(name, chairs_per_person))
